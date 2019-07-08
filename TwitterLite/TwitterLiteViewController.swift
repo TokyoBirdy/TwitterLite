ab41c5fe2bfa336479efe9b766a0d4cd4214f3c4
ceciliah <ceciliah@spotify.com>
2019-07-08 18:45:57 +0900

    Final project

---
+++ TwitterLite/TwitterLiteViewController.swift
@@ -0,0 +1,118 @@
+/// Copyright (c) 1 Reiwa Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+import UIKit
+
+class TwitterLiteViewController: UIViewController {
+  
+  @IBOutlet var tableView: UITableView!
+  
+  var viewModel: TwitterLiteViewModel!
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    viewModel = TwitterLiteViewModel(response: { [weak self] loadingStatus in
+      self?.displayTweets(loadingStatus: loadingStatus)
+    })
+    //viewModel.delegate = self
+    setupRefreshControl()
+  }
+  
+  private func setupRefreshControl() {
+    let refreshControl = UIRefreshControl()
+    refreshControl.addTarget(self, action: #selector(refreshData), for: .valueChanged)
+    tableView.refreshControl = refreshControl
+  }
+  
+  @objc private func refreshData() {
+    viewModel.loadTweets(loadingStatus: .subsequent)
+  }
+
+  private func displayTweets(loadingStatus: LoadingStatus) {
+    switch loadingStatus {
+    case .initial:
+      DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + 0.25) {
+        self.tableView.refreshControl?.endRefreshing()
+        self.tableView.reloadData()
+      }
+    case .subsequent:
+      tableView.refreshControl?.endRefreshing()
+      let indexes = (0..<viewModel.lastFetchedTweetsCount).map { IndexPath(row: $0, section: 0) }
+      UIView.animate(withDuration: 1, delay: 0.5, options: [], animations: {
+        self.tableView.beginUpdates()
+        self.tableView.insertRows(at: indexes, with: .fade)
+        self.tableView.endUpdates()
+      }, completion: nil)
+    }
+  }
+}
+
+extension TwitterLiteViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return viewModel.tweets.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Tweet", for: indexPath)
+    cell.textLabel?.text = viewModel.tweets[indexPath.row].text
+    return cell
+  }
+}
+
+extension TwitterLiteViewController: UITableViewDelegate {
+  func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+    return UITableView.automaticDimension
+  }
+  
+  func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+    return UITableView.automaticDimension
+  }
+}
+
+extension TwitterLiteViewController: UISearchBarDelegate {
+  func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+    searchBar.resignFirstResponder()
+  }
+  
+  func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+    if searchText == viewModel.emptyText {
+      viewModel.tweets = []
+      tableView.reloadData()
+    } else {
+      viewModel.searchContent = searchText
+      viewModel.loadTweets(loadingStatus: .initial)
+    }
+  }
+}
+
+//extension TwitterLiteViewController: TwitterLiteViewModelDelegate {
+//  func viewModel(_ viewModel: TwitterLiteViewModel,
+//                 didReceiveResponse loadingStatus: LoadingStatus) {
+//    displayTweets(loadingStatus: loadingStatus)
+//  }
+//}
---
+++ TwitterLite/TwitterLiteViewModel.swift
@@ -0,0 +1,77 @@
+/// Copyright (c) 1 Reiwa Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+//protocol TwitterLiteViewModelDelegate: class {
+//  func viewModel(_ viewModel: TwitterLiteViewModel,
+//                 didReceiveResponse loadingStatus: LoadingStatus)
+//}
+
+class TwitterLiteViewModel {
+
+  typealias CompletionHandler = (LoadingStatus) -> Void
+
+ // weak var delegate: TwitterLiteViewModelDelegate?
+
+  let emptyText = ""
+  let fetchLimit = 6
+  var searchContent: String
+  var lastFetchedTweetsCount: Int = 0
+  var tweets: [Tweet] = []
+  var response: CompletionHandler
+
+  init(response: @escaping CompletionHandler) {
+    self.searchContent = emptyText
+    self.response = response
+  }
+  // Mimic the behaviour of sending backend request
+  func loadTweets(loadingStatus: LoadingStatus) {
+    if loadingStatus == .initial {
+      tweets = []
+    }
+    let range = makeRange(withStartIndex: tweets.count)
+    let fetchedTweets = Array(fetchResults(basedOn: searchContent, range: range).reversed())
+    tweets = fetchedTweets + tweets
+    response(loadingStatus)
+   // delegate?.viewModel(self, didReceiveResponse: loadingStatus)
+  }
+
+  private func makeRange(withStartIndex startIndex: Int) -> Range<Int> {
+    let endIndex = startIndex + fetchLimit
+    return startIndex..<endIndex
+  }
+
+  private func fetchResults(basedOn text: String, range: Range<Int>) -> [Tweet] {
+    let searchResults = Backend.backendTweets.filter {
+      return $0.text.range(of: text, options: .caseInsensitive) != nil
+    }
+    let clampedRange = range.clamped(to: range.lowerBound..<searchResults.count)
+    let fetchResults = Array(searchResults[clampedRange])
+    lastFetchedTweetsCount = fetchResults.count
+    return fetchResults
+  }
+}
